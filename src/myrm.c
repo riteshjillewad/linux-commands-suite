46f7619b728b4ffa5e537d6e7c3efa7e18d27cdb
Ritesh Jillewad <130166456+riteshjillewad@users.noreply.github.com>
2026-02-17 19:42:07 +0530

    Add files via upload

---
+++ src/myrm.c
@@ -0,0 +1,52 @@
+/***************************************************************************************
+*   File name   :   myrm.c
+*   Description :   Custom implementation of linux 'rm' command
+*   Input       :   void
+*   Output      :   void
+*   Author      :   Ritesh Jillewad
+*   Date        :   17|02|2026
+***************************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define MAX_BUFFER_SIZE 1024
+
+/*
+    ALGORITHM:
+        The rm command is used to remove files
+        Step 1) Check if file is present using access()
+        Step 2) If present use the unlink() command to remove the file
+
+    USAGE:
+        ./mymv     fileName
+        argv[0]     argv[1]
+        
+        argc = 2
+*/
+
+int main(int argc, char *argv[]) 
+{
+    // Filters
+    if(argc != 2) 
+    {
+        printf("ERROR: Invalid number of arguments!\n");
+        printf("TRY: ./rm filename\n");
+        return -1;
+    }
+
+    // Use unlink() to remove the file
+    if(unlink(argv[1]) == 0)
+    {
+        printf("File removed successfully.\n");
+    }
+    else
+    {
+        printf("rm: cannot remove 'z.txt': No such file or directory");
+    }
+    
+    return 0;
+}
+ 
